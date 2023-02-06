949494d31a287c746c42d6b4a3107ca27e555f3f
Martin Boudreau <mboudrea\cstj.qc.ca>
2023-02-06 17:27:51 -0500

    preliminaire

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,26 @@
 #! /usr/bin/env node
-console.log("Hello, World!");
+
+const CP = require('child_process');
+
+function runCommand(command) {
+    try {
+        CP.execSync (command, {stdio: 'inherit'});
+    } catch (error) {
+        console.error(`Failed to execute '${command}'`, error);
+        return false;       
+    }
+    return true;
+}
+const repoName = process.argv[2] || 'js-app';
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/bobanum/create-js-app ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning '${repoName}'`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for '${repoName}'`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+
+console.log('Félicitations');
